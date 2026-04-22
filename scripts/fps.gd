90c027c34ff515a61e6efccc28bb79e45bb89bac
Sambit Kumar Mondal <sambitmondal02@gmail.com>
2026-04-22 23:23:17 +0530

    added fps and partial movement
    fps in main scene, movement in hero scene

--- scripts/fps.gd
+++ scripts/fps.gd
@@ -1,19 +1,13 @@
-extends Sprite2D
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
+	$Label.text = "FPS: " + str(Engine.get_frames_per_second())
 	pass
-	
-func _physics_process(delta: float) -> void:
-	if (Input.is_action_pressed("walk_right")):
-			position += Vector2(5, 0)
-	if (Input.is_action_pressed("walk_left")):
-			TEXTURE_REPEAT_MIRROR
-			position -= Vector2(5, 0)
